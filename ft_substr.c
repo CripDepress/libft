d1f2f2868d171652f593787ce5bfb11af564b69f
Akekonwit Keawdoung <akeawdou@z1t6c4.42bangkok.com>
2022-09-11 18:51:32 +0700

    ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: akeawdou <akeawdou@student.42bangkok.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/10 10:43:06 by akeawdou          #+#    #+#             */
-/*   Updated: 2022/09/11 18:25:13 by akeawdou         ###   ########.fr       */
+/*   Updated: 2022/09/11 18:48:18 by akeawdou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,10 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*ptr;
 
+	if (!s)
+		return (NULL);
+	if (len > (size_t)ft_strlen(s))
+		len = (size_t)ft_strlen(s);
 	ptr = (char *)malloc((len + 1) * sizeof(char));
 	if (ptr == NULL)
 		return (NULL);
